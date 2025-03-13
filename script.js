925b671691818e5ea6ae2e70dfd5f97cb8433a8a
cyokiocxzvx <166243320+cyokiocxzvx@users.noreply.github.com>
2025-03-13 13:28:02 +0100

    js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let age = 30;
+console.log(age);
+
+let name = "Дмитро";
+console.log(name);
+
+let isStudent = false;
+console.log(isStudent);
+
+let myString = "Майбутнє належить тим, хто вірить у красу своєї мрії.";
+console.log(myString);
+
+let myNumber = 20;
+myNumber += 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+let userName = prompt("Будь ласка, введіть ваше ім’я");
+alert("Вас звати: " + userName);
+console.log(typeof userName);
+
+let userAnswer = confirm("Ви справді бажаєте залишити цю сторінку?");
+console.log("Відповідь користувача: ", userAnswer);
+
+alert("Обережно! Це може бути ризиковано.");
+let confirmAction = confirm("Ви дійсно хочете продовжити?");
+console.log("Користувач підтвердив дію? ", confirmAction);
